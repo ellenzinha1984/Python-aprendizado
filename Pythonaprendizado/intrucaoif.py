4a9f182cc526e192054aa0b8b36f5731e8f5122f
Ellen Canossa <100133392+ellenzinha1984@users.noreply.github.com>
2022-07-21 14:47:33 -0300

    Update intrucaoif.py

--- Pythonaprendizado/intrucaoif.py
+++ Pythonaprendizado/intrucaoif.py
@@ -1,3 +1,9 @@
-preco = float(input('Digite o valor do produto: '))
-desc = preco - (preco * 0.2)
-print('O valor do produto com desconto é {:.2f}'.format(desc))
+#if é uma forma de avaliar e interpretar uma condição. Se for verdadeira, o bloco do if é executado.
+
+print('Exemplo de if - se nota for maior que 7, mostra mensagem de aprovado, senão reprovado.')
+
+nota = float(input('Digite a nota: '))
+if nota >= 7:
+    print('Aprovado!')
+else:
+    print('Reprovado!')
